5bcd2e5606b6c7a76350434dafe83422e8717c09
Julian Derungs <julian.derungs@primesystems.ch>
2021-01-28 16:59:43 +0100

    feat: add nuxt module

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { join } from "path";
+import consola from "consola";
+
+export default async function iconsModule({
+  prefix = "",
+  withConsole = false,
+} = {}) {
+  const logger = consola.withScope("@blokwise/icons");
+
+  this.nuxt.hook("components:dirs", (dirs) => {
+    dirs.push({
+      path: join(__dirname, "components"),
+      pattern: "**/*.vue",
+      prefix,
+    });
+  });
+
+  if (withConsole) {
+    logger.success({
+      message: "blokwise icons ready",
+      additional: `Module @blokwise/icons successfully registered.\nA bunch of amazing icons at your fingertips`,
+      badge: true,
+    });
+  }
+
+  return true;
+}
+
+module.exports.meta = require("./../package.json");
