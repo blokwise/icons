d30ec2fd3b012e92defb0584652c60868528ce8d
Julian Derungs <julian.derungs@primesystems.ch>
2021-04-12 13:05:12 +0200

    feat: remove wrapper around caution icon

--- src/components/indicative/Caution.vue
+++ src/components/indicative/Caution.vue
@@ -1,20 +1,18 @@
 <template>
-  <div>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      class="icon__caution"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-  </div>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    class="icon__caution"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
 </template>
 
 <script>
