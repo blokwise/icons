357704c54341fa46b2e2deb997486152b955a363
Julian Derungs <julian.derungs@primesystems.ch>
2021-04-13 15:37:36 +0200

    feat: add chevron right and chevron left

---
+++ src/components/control/ChevronRight.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    class="icon__chevron-right"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M9 5l7 7-7 7"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "ChevronRight",
+};
+</script>
